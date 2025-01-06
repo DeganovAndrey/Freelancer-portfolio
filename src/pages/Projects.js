da87852cd4823b07631b7f9b90594d8594708d23
DeganovAndrey <Deganovdyoosha@yandex.ru>
2025-01-06 21:07:05 +0300

    Added all routes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,9 +7,16 @@ const Projects = () => {
       <div className="container">
         <h2 className="title-1">Projects</h2>
         <ul className="projects">
-          {projects.map((project) => (
-            <Project title={project.title} img={project.img} />
-          ))}
+          {projects.map((project, index) => {
+            return (
+              <Project
+                key={index}
+                title={project.title}
+                img={project.img}
+                index={index}
+              />
+            );
+          })}
         </ul>
       </div>
     </main>
