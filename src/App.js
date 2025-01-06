0085fb2dcd01ac8abe1e421b79b3fd8fb6335613
DeganovAndrey <Deganovdyoosha@yandex.ru>
2025-01-06 19:22:37 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
 import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home/> */}
-      <Projects />
-      {/* <Contacts /> */}
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
